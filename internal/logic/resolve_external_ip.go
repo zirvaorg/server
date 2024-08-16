cbc7d04f82d795cd61c47b1a898c32ffc8817b13
Sercan Arğa <mail@sercanarga.com>
2024-08-16 18:41:46 +0300

    Refactor external IP resolution

--- internal/logic/resolve_external_ip.go
+++ internal/logic/resolve_external_ip.go
@@ -6,59 +6,60 @@ import (
 	"net/http"
 )
 
+var serviceUrls = []string{
+	"https://api.ipify.org",
+	"https://api.seeip.org",
+	"https://ifconfig.co/ip",
+	"https://ifconfig.io/ip",
+	"https://icanhazip.com",
+	"https://ident.me",
+	"https://myexternalip.com/raw",
+	"https://ipinfo.io/ip",
+	"https://icanhazip.com",
+	"https://ip.seeip.org",
+	"https://wtfismyip.com/text",
+	"https://ipecho.net/plain",
+	"https://icanhazip.com",
+	"https://ifconfig.me/ip",
+	"https://ip.tyk.nu",
+	"https://ipecho.net/plain",
+	"https://l2.io/ip",
+}
+
 func ResolveExternalIP() string {
-	urls := []string{
-		"https://api.ipify.org",
-		"https://api.seeip.org",
-		"https://ifconfig.co/ip",
-		"https://ifconfig.io/ip",
-		"https://icanhazip.com",
-		"https://ident.me",
-		"https://myexternalip.com/raw",
-		"https://wtfismyip.com/text",
-		"https://ipinfo.io/ip",
-		"https://ipecho.net/plain",
-		"https://icanhazip.com",
-		"https://ifconfig.me/ip",
-		"https://ip.tyk.nu",
-		"https://ipecho.net/plain",
-		"https://l2.io/ip",
-		"https://wtfismyip.com/text",
-		"https://icanhazip.com",
-		"https://ifconfig.me/ip",
-		"https://ip.seeip.org",
-		"https://ip.tyk.nu",
-		"https://ipecho.net/plain",
-		"https://l2.io/ip",
-		"https://wtfismyip.com/text",
-		"https://icanhazip.com",
-		"https://ifconfig.me/ip",
-		"https://ip.tyk.nu",
-		"https://ipecho.net/plain",
-	}
+	for i := 0; i < len(serviceUrls)-1; i += 2 {
+		ip1 := fetchIP(serviceUrls[i])
+		ip2 := fetchIP(serviceUrls[i+1])
 
-	for _, url := range urls {
-		resp, err := http.Get(url)
-		if err != nil {
-			continue
-		}
-		defer func(Body io.ReadCloser) {
-			err := Body.Close()
-			if err != nil {
-				return
-			}
-		}(resp.Body)
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			continue
-		}
-
-		ip := string(body)
-		if net.ParseIP(ip) != nil {
-			return ip
+		if ip1 != "" && ip1 == ip2 {
+			return ip1
 		}
 	}
 
 	return "[EXTERNAL_IP]"
+}
+
+func fetchIP(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		return ""
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			return
+		}
+	}(resp.Body)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	ip := string(body)
+	if net.ParseIP(ip) != nil {
+		return ip
+	}
+
+	return ""
 }
