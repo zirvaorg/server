578f85027ffe80e89cf413f28756cae7de52227d
Sercan Arğa <mail@sercanarga.com>
2024-08-25 00:39:42 +0300

    Remove MaxRedirects constant and add EchoData constant for clarity.

--- service/settings.go
+++ service/settings.go
@@ -3,7 +3,7 @@ package service
 import "time"
 
 const (
-	UserAgent    = "zirva.org/1.0"
-	MaxRedirects = 10
-	TimeOut      = 10 * time.Second
+	UserAgent = "zirva.org/1.0"
+	TimeOut   = 10 * time.Second
+	EchoData  = "ZIRVA-ORG-SERVICE-HELLO-MESSAGE-HERE-NOW" // 40 bytes
 )
